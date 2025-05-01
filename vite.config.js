2a8665aca9c4a26a474d49521b33bdbf4bcce0e9
Dipankar049 <dipg543@gmail.com>
2025-05-01 19:48:53 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/DSA-Simulator/',
+  base: '/dsa_simulator/',
   plugins: [react()],
 })
